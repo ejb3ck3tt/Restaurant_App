8ee9280ea6f677e5bac526c5efe8238e47528f22
Ethel Beckett <joy@192-168-1-104.tpgi.com.au>
2022-12-27 00:02:02 +1000

    setup react-frontend base

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
